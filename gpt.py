2ff5f410b2b5f823bdef2ce84f4f918aad3b9c66
deepmatics <satish@deepmatics-ai.com>
2026-03-02 20:56:41 +0530

    first commit

---
+++ gpt.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn
+from torch.nn import functional as F
+
+torch.manual_seed(1000)
+
+# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
