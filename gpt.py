153f22b918e63a18aa02845e1a1be9a7a3b3210e
deepmatics <satish@deepmatics-ai.com>
2026-03-02 21:09:07 +0530

    implemented char tokenizer

--- gpt.py
+++ gpt.py
@@ -5,5 +5,16 @@ from torch.nn import functional as F
 torch.manual_seed(1000)
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('input.txt', 'r', encoding='utf-8') as f:
+with open('data/input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
+
+chars = sorted(list(set(text)))
+
+s_to_i = {ch:i for i, ch in enumerate(chars)}
+i_to_s = {i:ch for i, ch in enumerate(chars)}
+
+encode = lambda s: [s_to_i[c] for c in s]
+decode = lambda l: ''.join(i_to_s[i] for i in l)
+
+print(encode("is this a GPT tokenizer?"))
+print(decode(encode("this is a GPT tokenizer?")))
